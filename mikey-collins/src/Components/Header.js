e5d6f0cae5d83b17d8af0e637156172692678176
CollinsDrew <collinsdrew1990@gmail.com>
2022-12-31 14:58:53 +0000

    responsive navbar styling done

---
+++ mikey-collins/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../Styles/Header.css";
+
+function Header() {
+  return (
+    <div className="mainContainer">
+      <h2 className="headerTitle">Mikey Collins</h2>
+      <h4 className="headerTitle">Music</h4>
+    </div>
+  );
+}
+
+export { Header };
