f3f8588a64af917c766a57eb18469fbcb1b6e732
CollinsDrew <collinsdrew1990@gmail.com>
2023-01-22 18:09:52 +0000

    projects array updated

--- mikey-collins/src/Components/More.js
+++ mikey-collins/src/Components/More.js
@@ -8,6 +8,12 @@ import potato from "../Images/potato.jpeg";
 import ramsden from "../Images/Ramsden.jpeg";
 import seazoo from "../Images/Seazoo.jpeg";
 import longy from "../Images/longy.jpeg";
+import alice from "../Images/aliceHubble.jpeg";
+import homechestra from "../Images/homechestra.jpeg";
+import almeria from "../Images/almeria.jpeg";
+import tigercats from "../Images/tigercats.jpeg";
+import virkelig from "../Images/virkelig.jpg";
+import trunks from "../Images/trunks.jpeg";
 
 function More() {
   const projects = [
@@ -52,6 +58,48 @@ function More() {
       image: longy,
       credit: "Producer & Engineer",
       link: "https://open.spotify.com/album/3byuP2smkehUGTnlWwHzlz?si=utNAeasiS9itJX1h0lc2dw&nd=1",
+    },
+    {
+      artist: "Alice Hubble",
+      title: "'Hexentanzplatz'",
+      image: alice,
+      credit: "Engineer & Programming",
+      link: "https://open.spotify.com/album/7xaVhmgZ8IqVnWrBcdd971?si=0X00addTQ5OG-uRIwPpcUQ&nd=1",
+    },
+    {
+      artist: "Homechestra",
+      title: "'Dialogues that never happened'",
+      image: homechestra,
+      credit: "",
+      link: "https://open.spotify.com/album/1WBmZNPcF1BXjeAgwNh3Dp?si=SaVvH5blQ5uy_KaUxCNvHg&nd=1",
+    },
+    {
+      artist: "Jessica Lauren",
+      title: "'Almeria'",
+      image: almeria,
+      credit: "",
+      link: "https://open.spotify.com/album/0eRpTXh6KpWVOQIqi5vKYk?si=ZkVoKzjUQXSXZVHjm0AbFw&nd=1",
+    },
+    {
+      artist: "Tigercats",
+      title: "'Pig City'",
+      image: tigercats,
+      credit: "",
+      link: "https://open.spotify.com/album/49HuHrnIY6v9ecgSe5jZUo?si=TAK9_aFJTIOXmZH06jj-yg&nd=1",
+    },
+    {
+      artist: "Virkelig",
+      title: "'Lengsel blir til gjemsel'",
+      image: virkelig,
+      credit: "",
+      link: "https://open.spotify.com/album/3byuP2smkehUGTnlWwHzlz?si=utNAeasiS9itJX1h0lc2dw&nd=1",
+    },
+    {
+      artist: "Seazoo",
+      title: "'Trunks'",
+      image: trunks,
+      credit: "Mixed & Mastered",
+      link: "https://open.spotify.com/album/39TE0lEmtUCgLz6yWygYnO?si=NiwnRGWTQCG2xqhL2dugzQ&nd=1",
     },
   ];
 
