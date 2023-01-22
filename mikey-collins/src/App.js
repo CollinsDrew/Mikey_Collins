d82e8f7b29fcf7c257bfab48a62ac6ad19587a8a
CollinsDrew <collinsdrew1990@gmail.com>
2023-01-22 13:26:52 +0000

    home and about responsiveness sorted

--- mikey-collins/src/App.js
+++ mikey-collins/src/App.js
@@ -29,7 +29,7 @@ function App() {
           <Route path="/press" element={<Press />} />
           <Route path="/more" element={<More />} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </BrowserRouter>
   );
